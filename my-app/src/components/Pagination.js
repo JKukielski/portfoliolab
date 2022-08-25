1e3d8eb48360f87e10644a38c06a85aada4d55da
Jasiu Kukielski <jankukielski1@gmail.com>
2022-08-25 19:08:41 +0200

    functionality of pagination completed - without styling

--- my-app/src/components/Pagination.js
+++ my-app/src/components/Pagination.js
@@ -1,23 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import "../scss/pagination.scss";
-import PaginationElements from "./PaginationElements";
+import { masterList } from "../constants/listItems";
 
 const Pagination = () => {
+  const [activeTab, setActiveTab] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(3);
+
   return (
     <div className="app__pagination">
       <div className="app__pagination-buttons">
-        <button className="app__pagination-btn">Fundacjom</button>
-        <button className="app__pagination-btn">
+        <button
+          onClick={() => {
+            setActiveTab(1);
+            setCurrentPage(1);
+          }}
+          className="app__pagination-btn"
+        >
+          Fundacjom
+        </button>
+        <button
+          onClick={() => {
+            setActiveTab(2);
+            setCurrentPage(1);
+          }}
+          className="app__pagination-btn"
+        >
           Organizacjom <br /> pozarządowym
         </button>
-        <button className="app__pagination-btn">Lokalnym zbiórkom</button>
+        <button
+          onClick={() => {
+            setActiveTab(3);
+            setCurrentPage(1);
+          }}
+          className="app__pagination-btn"
+        >
+          Lokalnym zbiórkom
+        </button>
       </div>
       <div className="app__pagination-description">
         W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi
         współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego
         potrzebują.
       </div>
-      <PaginationElements />
+      <ul className="app__pagination-list">
+        {masterList[activeTab - 1]
+          .slice(perPage * currentPage - perPage, perPage * currentPage)
+          .map((element, index) => {
+            return (
+              <li key={index}>
+                <h1 className="app__pagination-list_element-heading">
+                  {element.name}
+                </h1>
+                <p className="app__pagination-list_element-description">
+                  {element.description}
+                </p>
+                <p className="app__pagination-list_element-info">
+                  {element.info}
+                </p>
+              </li>
+            );
+          })}
+      </ul>
+      <div className="app__pagination-numbers">
+        {Array(Math.ceil(masterList[activeTab - 1].length / perPage))
+          .fill(1)
+          .map((element, index) => {
+            return (
+              <button
+                className="app__pagination-numbers_btn"
+                onClick={() => setCurrentPage(index + 1)}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+      </div>
     </div>
   );
 };
---
+++ my-app/src/constants/listItems.js
@@ -0,0 +1,101 @@
+export const masterList = [
+  [
+    {
+      name: 'Fundacja: "Dbam o Zdrowie" ',
+      description:
+        "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+      info: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+    },
+    {
+      name: "Fundacja “Dla dzieci” ",
+      description: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+      info: "ubrania, meble, zabawki",
+    },
+    {
+      name: "Fundacja “Bez domu”” ",
+      description:
+        "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+      info: "ubrania, jedzenie, ciepłe koce",
+    },
+    {
+      name: 'Fundacja: "Dbam o Zdrowie" ',
+      description:
+        "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+      info: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+    },
+    {
+      name: 'Fundacja: "xyz" ',
+      description:
+        "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+      info: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+    },
+    {
+      name: 'Fundacja: "abc" ',
+      description:
+        "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+      info: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+    },
+    {
+      name: 'Fundacja: "def" ',
+      description:
+        "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+      info: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+    },
+    {
+      name: 'Fundacja: "ghi" ',
+      description:
+        "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+      info: "ubrania, jedzenie, sprzęt AGD, meble, zabawki",
+    },
+  ],
+  [
+    {
+      name: "Organizacja “Lorem Ipsum 1” ",
+      description:
+        "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+      info: "Ut, aliquam, purus, sit, amet",
+    },
+    {
+      name: "Organizacja “Lorem Ipsum 2” ",
+      description:
+        "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+      info: "Ut, aliquam, purus, sit, amet",
+    },
+    {
+      name: "Organizacja “Lorem Ipsum 3” ",
+      description: "Scelerisque in dictum non consectetur a erat nam.",
+      info: "Mi, quis, hendrerit, dolor",
+    },
+    {
+      name: "Organizacja “Lorem Ipsum 4” ",
+      description:
+        "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+      info: "Ut, aliquam, purus, sit, amet",
+    },
+    {
+      name: "Organizacja “Lorem Ipsum 5” ",
+      description:
+        "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+      info: "Ut, aliquam, purus, sit, amet",
+    },
+  ],
+  [
+    {
+      name: "Zbiórka “Lorem Ipsum 1” ",
+      description:
+        "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+      info: "Egestas, sed, tempus",
+    },
+    {
+      name: "Zbiórka “Lorem Ipsum 2” ",
+      description:
+        "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+      info: "Ut, aliquam, purus, sit, amet",
+    },
+    {
+      name: "Zbiórka “Lorem Ipsum 3” ",
+      description: "Scelerisque in dictum non consectetur a erat nam.",
+      info: "Mi, quis, hendrerit, dolor",
+    },
+  ],
+];
